5afd2acc7e9915af494a8b0a952b9bbc1a3d4aa3
Huarong Luo <henry.luohr@shopee.com>
2022-07-15 18:07:45 +0800

    feat: convert tab to space

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -16,6 +16,7 @@ vim.opt.autoindent      = true
 vim.opt.tabstop         = 4
 vim.opt.shiftwidth      = 4
 vim.opt.softtabstop     = 4
+vim.opt.expandtab       = true
 
 require("core.keymaps")
 require("core.plugins")
